8d72317ce257e5cd816c4619b299c26a7d00c798
gabriel-g-v-a <106846266+gabriel-g-v-a@users.noreply.github.com>
2022-11-20 21:14:29 -0500

    Add files via upload

---
+++ Ejercicio_8/Multiplos.cs
@@ -0,0 +1,10 @@
+class Multiplos{
+    public int n;
+    public void OPmultiplos(){;
+        for (n=1; n<=3000; n++){
+            if (n % 3==0){
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Multiplos R=new Multiplos();
+        R.OPmultiplos();
+    }
+}
